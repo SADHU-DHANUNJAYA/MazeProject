c7e00735508843c6fce4aa93575459c6d0510f55
SADHU <>
2023-01-05 18:29:58 +0530

    MazaSolverProject

---
+++ src/main/java/com/mycompany/mazasolverproject/MazaSolverProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mazasolverproject;
+
+/**
+ *
+ * @author 91939
+ */
+public class MazaSolverProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
